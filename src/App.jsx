981a3fd6a1d2c05c4da99475d4c1d960a2f2932e
adrielhenry33 <86744909+adrielhenry33@users.noreply.github.com>
2023-07-26 23:07:17 -0300

    aula 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { Post } from './Post'
-import {Header} from './components/Header'
+import { Header } from './Components/Header';
+import { Post } from './Post';
+
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+
+
+      <div className={styles.wrapper}>
+       <aside> 
+        sidebar
+       </aside>
+       <main>
+        <Post
+          author="Diego Fernandes"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+        />
+        <Post
+          author="Gabriel Buzzi"
+          content="Um novo post muito legal"
+        />
+    
+       </main>
+      </div>
     </div>
   );
 }
+/*<aside> signica colocar de lado, tudo o que
+estiver entre os asides fica lateralmente na 
+pagina*/
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 // iremos simular o cabecalho da logo ignite
 import styles from './Header.module.css'; // importacao do css exclusivo do cabecalho
+import igniteLogo from '../imagens/igniteLogo.svg';//importacao da imagem do cabecalho
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong> Ignite feed </strong>
+      <img src= {igniteLogo} alt= "logotipo do ignite " />
     </header>
   );
 }
