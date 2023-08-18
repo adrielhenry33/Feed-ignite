4af4bc7373e9dfe3668d9aedc107b4033a1b7afa
adrielhenry33 <86744909+adrielhenry33@users.noreply.github.com>
2023-08-18 14:13:51 -0300

    finalizando a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar()
@@ -7,12 +9,14 @@ export function Sidebar()
         <img className={styles.cover} src='https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=256&q=72'
         />
         <div className={styles.profile}>
+          <img className={styles.avatar} src='https://github.com/adrielhenry33.png'/>
           <strong> Adriel Henry</strong>
           <span>Web developer</span>
         </div>
 
         <footer>
           <a href='#'>
+            <PencilLine></PencilLine>
             Editar seu perfil
           </a>
         </footer>
